d8e59b1030a976427a4bee766fd12cea7c6dc649
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2026-07-21 00:55:51 +0100

    fix: format import statements for consistency in AppRoutes component

--- summer-camp/src/AppRoutes.tsx
+++ summer-camp/src/AppRoutes.tsx
@@ -1,13 +1,13 @@
 // import React from "react";
 import { Routes, Route } from "react-router-dom";
-import App from "./App";
-import AdminDashboard from "./pages/AdminDashboard";
+import App from "./App"; 
+import AdminDashboard from "./pages/AdminDashboard"; 
 
 export default function AppRoutes() {
   return (
     <Routes>
-      
       <Route path="/" element={<App />} />
+
       {/* Admin Dashboard Table View */}
       <Route path="/admin" element={<AdminDashboard />} />
       <Route path="/registrations" element={<AdminDashboard />} />
